a5afe66ba7f5d1a622bb8cba5d417475541f5652
Mohammed Awad <momoman2029@gmail.com>
2026-06-28 21:21:07 +0300

    Update Checkout.jsx

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -20,7 +20,7 @@ export default function Checkout() {
 
   // ⭐ Stripe Checkout
   const handlePayment = async () => {
-    const res = await fetch("http://localhost:3000/create-checkout-session", {
+    const res = await fetch("https://nozha.onrender.com/create-checkout-session", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ cart })
